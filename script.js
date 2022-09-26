c5d96d304c5d8c565a8750ffda32842a4c594070
jordanforbes <jordforb@gmail.com>
2022-09-26 10:01:00 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(()=>{
+    //root
+    $(".0").addClass("rootNote")
+
+    $(".1, .2").addClass("secondNote")
+
+    $(".3, .4").addClass("thirdNote")
+
+    $(".5").addClass("fourthNote")
+    $(".6").addClass("tritoneNote")
+    $(".7").addClass("fifthNote")
+
+    $(".10, .11").addClass("seventhNote")
+
+})
