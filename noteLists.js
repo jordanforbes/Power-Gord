7a4514bb15c75017cdca6a536835d41f821a325e
jordanforbes <jordforb@gmail.com>
2022-09-26 19:05:38 -0400

    chord dictionary

--- noteLists.js
+++ noteLists.js
@@ -3,9 +3,37 @@ intervals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 notes = ["A", "A#", "B", "C","C#", "D","D#", "E", "F","F#", "G", "G#"]
 
 //chord lists
-chords=[
-        [0,7], //power chord
-        [0,4,7],[0,5,7], //major/minor triad chords
-        [0,4,7,10],[0,4,7,11], //7th chords
-        [0,5,7,10],[0,5,7,11]
-        ]
+chords={
+        "diad":{
+            "power":[0,7]
+        }, //power chord
+        "triad":{ //major/minor triad chords
+            "major":[0,4,7],
+            "minor":[0,3,7],
+            "augmented":[0,4,8],
+            "diminished":[0,3,6],
+        },
+        "seventh":{ //7th chords
+            "dominant":[0,4,7,10],
+            "major":[0,4,7,11],
+            "minor":[0,3,7,10],
+            "minorflat5":[0,3,6,10],
+            "diminished":[0,3,6,9]
+        }
+
+    }
+
+// console.log(notes[0],notes[7])
+// console.log(chords["power"])
+
+const chordSearch=(chordInts)=>{
+    console.log(chordInts)
+    for (const i in chordInts){
+        console.log(notes[chordInts[i]])
+    }
+};
+
+chordSearch(chords["triad"]["major"])
+chordSearch(chords["triad"]["minor"])
+chordSearch(chords["triad"]["diminished"])
+chordSearch(chords["seventh"]["diminished"])
