b7ef88d47aae6ce3be8c5d9ca8ec20265f84523f
jordanforbes <jordforb@gmail.com>
2022-09-28 20:10:04 -0400

    keySelect test

--- powergord/src/scripts/keySelect.js
+++ powergord/src/scripts/keySelect.js
@@ -2,10 +2,9 @@ const nl = require('./noteLists')
 
 // returns array with the notes in order starting from the key's root note
 let keySelect=(root="C")=>{
-    let key = nl.notes
-    // console.log("initial key array "+key)
-    let rootIndex = key.indexOf(root)
-    if (key[0] == root){
+    let key = nl.notes //secondary initial array
+    let rootIndex = key.indexOf(root) //find root
+    if (key[0] == root){ //if key is A, nothing has to be done
         return key
     }else{
         for(i = 0; i < rootIndex; i++){
--- powergord/src/tests/keySelect.test.js
+++ powergord/src/tests/keySelect.test.js
@@ -1,5 +1,61 @@
 const ks = require('../scripts/keySelect')
 
+//A
+test('inserting the root note A should return an array with the notes starting from A',()=>{
+    expect(ks.keySelect("A")).toEqual(['A', 'A#', 'B', 'C', 'C#','D', 'D#', 'E', 'F', 'F#', 'G', 'G#'])
+});
+
+//A#
+test('inserting the root note A# should return an array with the notes starting from A#',()=>{
+    expect(ks.keySelect("A#")).toEqual(['A#','B', 'C', 'C#','D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A'])
+});
+
+//B
+test('inserting the root note B should return an array with the notes starting from B',()=>{
+    expect(ks.keySelect("B")).toEqual(['B', 'C', 'C#','D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#'])
+});
+
+//C
+test('inserting the root note C should return an array with the notes starting from C',()=>{
+    expect(ks.keySelect("C")).toEqual(['C', 'C#','D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#','B'])
+});
+
+//C#
+test('inserting the root note C should return an array with the notes starting from C#',()=>{
+    expect(ks.keySelect("C#")).toEqual(['C#','D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#','B','C'])
+});
+
+//D
 test('inserting the root note D should return an array with the notes starting from D',()=>{
     expect(ks.keySelect("D")).toEqual(['D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#'])
+});
+
+//D#
+test('inserting the root note D should return an array with the notes starting from D#',()=>{
+    expect(ks.keySelect("D#")).toEqual(['D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#','D'])
+});
+
+//E
+test('inserting the root note E should return an array with the notes starting from E',()=>{
+    expect(ks.keySelect("E")).toEqual(['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#','D','D#'])
+});
+
+//F
+test('inserting the root note F should return an array with the notes starting from F',()=>{
+    expect(ks.keySelect("F")).toEqual(['F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#','D','D#','E'])
+});
+
+//F#
+test('inserting the root note F# should return an array with the notes starting from F#',()=>{
+    expect(ks.keySelect("F#")).toEqual(['F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#','D','D#','E','F'])
+});
+
+//G
+test('inserting the root note G should return an array with the notes starting from G',()=>{
+    expect(ks.keySelect("G")).toEqual(['G', 'G#', 'A', 'A#', 'B', 'C', 'C#','D','D#','E','F','F#'])
+});
+
+//G#
+test('inserting the root note G# should return an array with the notes starting from G#',()=>{
+    expect(ks.keySelect("G#")).toEqual(['G#', 'A', 'A#', 'B', 'C', 'C#','D','D#','E','F','F#','G'])
 });
