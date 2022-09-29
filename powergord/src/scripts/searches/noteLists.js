1d10fb133189f61c9ac594e525b7c99ba4daced0
jordanforbes <jordforb@gmail.com>
2022-09-29 11:43:38 -0400

    scale dictionary

--- powergord/src/scripts/searches/noteLists.js
+++ powergord/src/scripts/searches/noteLists.js
@@ -1,27 +1,49 @@
+////////////////////////////////////////////////////////////////
+//1. assign notes to modes
+//2. the chords and scales will change the kind they are based on their modes
+////////////////////////////////////////////////////////////////
+
 // note lists
 const intervals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 const notes = ["A", "AB", "B", "C","CD", "D","DE", "E", "F","FG", "G", "GA"]
 
 
+
+//mode dictionary
+const scales = {
+    "major":{//modes                 //scale degrees
+        "ionian":[0,2,4,5,7,9,11],      //1
+        "lydian":[0,2,4,6,7,9,11],      //4
+        "mixolydian":[0,2,4,5,7,9,10]   //5
+    },
+    "minor":{//modes
+        "dorian":[0,2,3,5,7,9,10],      //2
+        "phrygian":[0,1,3,5,7,8,10],    //3
+        "aeolian":[0,2,3,5,7,9,10],     //6
+        "locrian":[0,1,3,5,6,8,10]      //7
+    }
+}
+
+
+
 //chord dictionary
 const chords={
-    "diad":{"third":[0,2],"power":[0,4]},
-    "triad":{"majMin":[0,2,4],"aug":[0,2,5]},
-    "quadrad":{"sev":[0,2,4,6]}
+    "diad":{
+        "third":[0,2],
+        "power":[0,4]
+    },
+    "triad":{
+        "majMin":[0,2,4],
+        "aug":[0,2,5]
+    },
+    "quadrad":{
+        "sev":[0,2,4,6]
+    }
 }
 
-const modes = {
-    "Ionian":[0,2,4,5,7,9,11],
-    "Dorian":[0,2,3,5,7,9,10],
-    "Phrygian":[0,1,3,5,7,8,10],
-    "Lydian":[0,2,4,6,7,9,11],
-    "Mixolydian":[0,2,4,5,7,9,10],
-    "Aeolian":[0,2,3,5,7,9,10],
-    "Locrian":[0,1,3,5,6,8,10]
 
-}
 
-module.exports ={intervals, notes, modes, chords}
+module.exports ={intervals, notes, scales, chords}
 
 
 //old
