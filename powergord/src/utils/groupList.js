b2cc44244ac649d22341a0e5e85a80fa64ece245
jordanforbes <jordforb@gmail.com>
2022-09-30 14:06:33 -0400

    refactoring grouping list

---
+++ powergord/src/utils/groupList.js
@@ -0,0 +1,20 @@
+// note List
+
+const scale= {
+    "ionian":[0,2,4,5,7,9,11],
+    "dorian":[0,2,3,5,7,9,10],
+    "phrygian":[0,1,3,5,7,8,10],
+    "lydian":[0,2,4,6,7,9,11],
+    "mixolydian":[0,2,4,5,7,9,10],
+    "aeolian":[0,2,3,5,7,9,10],
+    "locrian":[0,1,3,5,6,8,10]
+}
+
+const chord= {
+    "tri":[0,2,4], //major/minor triad
+    "augTri":[0,2,5], //augmented triad
+    "sev":[0,2,4,6], //seventh quadrad
+    "pow":[0,4]   //power chord (5 chord)
+}
+
+module.exports={scale, chord}
--- powergord/src/utils/musicFunctions.js
+++ powergord/src/utils/musicFunctions.js
@@ -5,68 +5,43 @@ var notes= ["c", "cs", "d", "ds", "e", "f", "fs", "g", "gs", "a", "as", "b"]
 
 var notesForLookup = ["C", "CD", "D", "DE", "E", "F", "FG", "G", "GA", "A", "AB", "B"]
 
+gl = require('./groupList')
+const scale = gl.scale
+const chord = gl.chord
 
-//scale list
-const scale= {
-        "ionian":[0,2,4,5,7,9,11],
-        "dorian":[0,2,3,5,7,9,10],
-        "phrygian":[0,1,3,5,7,8,10],
-        "lydian":[0,2,4,6,7,9,11],
-        "mixolydian":[0,2,4,5,7,9,10],
-        "aeolian":[0,2,3,5,7,9,10],
-        "locrian":[0,1,3,5,6,8,10]
+//converts the number strings to int
+const scaleParse=(modeName)=>{
+    numScale= []
+    for (i in scale[modeName]){
+        step = parseInt(i)
+        numScale.push(step)
     }
-
-const chord= {
-    0:[0,2,4], //major/minor triad
-    1:[0,2,5], //autmented triad
-    2:[0,2,4,6], //seventh quadrad
-    3:[0,4]   //power chord (5 chord)
+    return numScale
 }
-console.log("io test")
-// console.log(typeof scale["ionian"])
-
-
-testArr = []
-for (i in scale["ionian"]){
-    step = parseInt(i)
-    testArr.push(step)
-}
-console.log(testArr)
-
-
-
-//major/minor scales
-var majorScales = ["ionian", "lydian", "mixolydian"];
-var minorScales = ["dorian", "phrygian", "aeolian", "locrian"];
 
 //notes finder. Gets called for each note
-function noteFinder(n,modeName,chord){
+const noteFinder=(root,modeName,chord)=>{
 
-    // console.log(modeName)
-    brokenMode = scale["ionian"]
-    // var mode = scale[modeName]
-    mode = [0,2,4,5,7,9,11,"Ionian"]
-    // console.log("broken mode "+brokenMode.type)
-  var scaleArr=[]
+    let mode = scaleParse("ionian")
+    let scaleArr=[]
 
-  var x = notes.indexOf(n)
+    let rootNote = notes.indexOf(root)
 
     //mode notes finder
-  mode.forEach(function(element) {
+    mode.forEach((element) => {
 
-    element=element + x;
+        element += rootNote;
 
-    if(element<12){
-    scaleArr.push(notes[element]);
-    element=element - x
+        if(element<12){
+        scaleArr.push(notes[element]);
+        element=element - rootNote
 
-  }else if(element>=12){
-    element=element-12;
-    scaleArr.push(notes[element]);
-    element=element+x+12
+    }else if(element>=12){
+        element=element-12;
+        scaleArr.push(notes[element]);
+        element=element+rootNote+12
 
-  }
+    }
 
   });
     //chord notes finder
@@ -80,7 +55,7 @@ function noteFinder(n,modeName,chord){
     })
       finArr= chordArr
     }else{
-      console.log(n.toUpperCase() + " "+ modeName + " scale")
+      console.log(root.toUpperCase() + " "+ modeName + " scale")
       console.log(scaleArr)
     }
 
@@ -91,7 +66,7 @@ function noteFinder(n,modeName,chord){
 
 function Maj7(n){
   finArr=[]
-  noteFinder(n,scale["ionian"],chord[2])
+  noteFinder(n,scale["ionian"],chord["sev"])
   console.log(finArr)
   return finArr
 }
@@ -99,35 +74,35 @@ function Maj7(n){
 function Maj(n){
   finArr=[]
   //return n.toUpperCase() + " Major"
-  noteFinder(n,scale["ionian"],chord[0])
+  noteFinder(n,scale["ionian"],chord["tri"])
   console.log(finArr)
   return finArr
 }
 
 function Min(n){
   finArr=[]
-  noteFinder(n,scale["aeolian"],chord[0])
+  noteFinder(n,scale["aeolian"],chord["tri"])
   console.log(finArr)
   return finArr
 }
 
 function AugTri(n){
   finArr=[]
-  noteFinder(n,scale["lydian"],chord[1])
+  noteFinder(n,scale["lydian"],chord["augTri"])
   console.log(finArr)
   return finArr
 }
 
 function DimTri(n){
   finArr=[]
-  noteFinder(n,scale["locrian"],chord[0])
+  noteFinder(n,scale["locrian"],chord["tri"])
   console.log(finArr)
   return finArr
 }
 
 function fiveDyad(n){
   console.log(n.toUpperCase() + " Power Chord")
-  noteFinder(n,majorScales[0],chord[3])
+  noteFinder(n,scale["ionian"],chord["pow"])
 }
 
 function findNoteIndex(n){
