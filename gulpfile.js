04bf5cbc3d49649505f64f7fad1276bc9e68d7ec
Jordan Forbes <jordforb@gmail.com>
2022-12-09 16:08:47 -0500

    gulp isn't very helpful to me right now

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,27 @@
 const gulp = require('gulp');
 const git = require('gulp-git')
 
-// gulp.task =define tasks
-// gulp.src =point to files to use
-// gulp.dest = point to folder to output
-// gulp.watch = watch files and folders for changes
+// // gulp.task =define tasks
+// // gulp.src =point to files to use
+// // gulp.dest = point to folder to output
+// // gulp.watch = watch files and folders for changes
 
-// logs message
-gulp.task('message', async ()=>{
-    return console.log('gulp is running')
-})
+// // logs message
+// gulp.task('message', async ()=>{
+//     return console.log('gulp is running')
+// })
 
-// Run git add
-// src is the file(s) to add (or ./*)
-// gulp.task('add', function(){
-//     return gulp.src('./*')
-//       .pipe(git.add());
+// // Run git add
+// // src is the file(s) to add (or ./*)
+// // gulp.task('add', function(){
+// //     return gulp.src('./*')
+// //       .pipe(git.add());
+// //   });
+
+//   // Run git add with options
+//   gulp.task('add', function(){
+//     return gulp.src('./git-test/*')
+//       .pipe(git.add({args: ' .'}));
 //   });
 
-  // Run git add with options
-  gulp.task('add', function(){
-    return gulp.src('./git-test/*')
-      .pipe(git.add({args: ' .'}));
-  });
-
-  // Run git commit with options
-gulp.task('commit', function(){
-    return gulp.src('./git-test/*')
-      .pipe(git.commit('initial commit', {args: '-a -m'}));
-  });
-//blah
+//   // Run git commit with options
