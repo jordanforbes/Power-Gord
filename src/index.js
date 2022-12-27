faf2d1c7103cad410c6f972bda0f9176b3796969
Jordan Forbes <jordforb@gmail.com>
2022-12-27 11:12:24 -0500

    created apicall branch

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './app/store';
 import { Provider } from 'react-redux';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
